60bf21720c4a8b9a1de34f911c4730ff20c25725
Chrisfaherty <chrismactom@gmail.com>
2023-09-08 19:20:09 +0000

    Test deployment

--- greenways/settings.py
+++ greenways/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['ci-greenways-a76ef9e9c4a6.herokuapp.com']
 
 
 # Application definition
