2b6390fff688a006d64fc4fc2966edcd55fb1aaf
Chrisfaherty <chrismactom@gmail.com>
2023-09-08 19:36:21 +0000

    Test deployment

--- greenways/settings.py
+++ greenways/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'greenways.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+
+ DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+  }
+ }
+
+#DATABASES = {
+   #  'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#}
 
 
 
